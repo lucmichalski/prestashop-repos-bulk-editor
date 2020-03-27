a064ea2ff2570728e454438bb8338af3917be482
matks <mathieu.ferment@prestashop.com>
2020-03-27 14:14:54 +0100

    Better

--- run.php
+++ run.php
@@ -9,18 +9,24 @@ $client->authenticate($token, null, Github\Client::AUTH_HTTP_TOKEN);
 $content = file_get_contents(__DIR__.'/template.txt');
 $path = '.github/PULL_REQUEST_TEMPLATE.md';
 
+// to iterate
+$repositoryName = 'ps_shoppingcart';
+
 $commitMessage = 'Add Pull Request template for github';
-$branch = 'dev';
+$branch = 'master';
+// @todo: check I forked repository before
+// @todo: check on my fork there is a master or dev branch
 $committer = array('name' => 'matks', 'email' => 'mathieu.ferment@prestashop.com');
 
-// $fileInfo = $client->api('repo')->contents()
-//    ->create('matks', 'ps_facetedsearch', $path, $content, $commitMessage, $branch, $committer);
+$fileInfo = $client->api('repo')->contents()
+    ->create('matks', $repositoryName, $path, $content, $commitMessage, $branch, $committer);
 
-$message = 'This pull request a GitHub template for Pull Requests'.PHP_EOL.PHP_EOL.'This PR is created automatically.';
+$message = 'This pull request a GitHub template for Pull Requests'.PHP_EOL.PHP_EOL.'This PR is created automatically'
+    . ' by [Matks PrestaShop Repositories Bulk Editor](https://github.com/matks/prestashop-repos-bulk-editor)';
 
-$pullRequest = $client->api('pull_request')->create('prestashop', 'ps_facetedsearch', array(
+$pullRequest = $client->api('pull_request')->create('prestashop', $repositoryName, array(
     'base'  => 'master',
-    'head'  => 'matks:dev',
+    'head'  => 'matks:'.$branch,
     'title' => 'Add GitHub PR template',
     'body'  => $message
 ));
