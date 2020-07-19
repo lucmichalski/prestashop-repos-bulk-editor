e7187dc64b82dbf595080fe71bf5f0b117cffde6
matks <mathieu.ferment@prestashop.com>
2020-07-19 14:59:01 +0200

    Add core weekly

---
+++ get-latest-releases.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/util.php';
+
+// -------------- SETUP -------------- //
+
+$client = new \Github\Client();
+$token = file_get_contents(__DIR__ . '/token.txt');
+$client->authenticate($token, null, Github\Client::AUTH_HTTP_TOKEN);
+
+$organizationApi = $client->api('organization');
+
+$paginator = new Github\ResultPager($client);
+$parameters = array('prestashop');
+$result = $paginator->fetchAll($organizationApi, 'repositories', $parameters);
+
+$releasesData = [];
+$filterOnlyLastWeek = true;
+$oneWeekAgo = new DateTime();
+$oneWeekAgo->modify('- 7 day');
+
+$i = 0;
+
+foreach ($result as $repository) {
+    $repoName = $repository['name'];
+
+    try {
+        $release = $client->api('repo')->releases()->latest(
+            'prestashop', $repoName
+        );
+
+        $publishedAt = \DateTime::createFromFormat(
+            \DateTime::RFC3339,
+            $release['published_at']
+        );
+
+        if ($filterOnlyLastWeek) {
+            if ($publishedAt < $oneWeekAgo) {
+                continue;
+            }
+        }
+
+        $releasesData[$repoName] = [
+            'repository' => $repoName,
+            'url' => $release['html_url'],
+            'version' => $release['name'],
+            'published_at' => $publishedAt
+        ];
+
+    } catch (\Github\Exception\RuntimeException $e) {
+        if ($e->getCode() !== 404) {
+            throw $e;
+        }
+    }
+
+    $i++;
+}
+
+$title = '## Releases'.PHP_EOL.PHP_EOL;
+foreach ($releasesData as $release) {
+    $title.= sprintf(
+        '* [%s](%s): [%s](%s)',
+        ucfirst($release['repository']),
+        'https://github.com/PrestaShop/'.$release['repository'],
+        $release['version'],
+        $release['url']
+    );
+    $title.= PHP_EOL;
+}
+
+echo $title;
+
